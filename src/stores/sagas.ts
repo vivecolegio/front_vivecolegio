c59587fc863cfa3b735d59909142086afec36fcb
MasterVHM <scrummaster@vhmsoluciones.com>
2022-03-16 23:32:42 -0500

    update

--- src/stores/sagas.ts
+++ src/stores/sagas.ts
@@ -2,8 +2,8 @@ import { all } from 'redux-saga/effects';
 // import authSagas from './auth/saga';
 // import todoSagas from './todo/saga';
 import chatSagas from './actions/Chat/saga'
-import surveyListSagas from './actions/Survey/surveyList/saga';
-import surveyDetailSagas from './actions/Survey/surveyDetail/saga';
+import surveyListSagas from './actions/Survey/SurveyList/saga';
+import surveyDetailSagas from './actions/Survey/SurveyDetail/saga';
 
 export default function* rootSaga() {
   yield all([
