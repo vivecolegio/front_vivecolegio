55248f43368eb9623d46509f76b556d1ff230375
MasterVHM <scrummaster@vhmsoluciones.com>
2022-09-15 19:31:37 -0500

    update

--- src/AppRenderer.tsx
+++ src/AppRenderer.tsx
@@ -6,13 +6,13 @@ import App from './routes/App';
 import configureStore from './stores/configureStore';
 
 // const App = React.lazy(()=> {return import(/* webpackChunkName: "App" */ './routes/App')})
-
 const { store, persistor } = configureStore();
-
 const container = document.getElementById('app');
-
 // Create a root.
 const root = ReactDOMClient.createRoot(container);
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 root.render(
   <Provider store={store}>
@@ -25,3 +25,12 @@ root.render(
 );
 
 // reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals("");
--- src/index.tsx
+++ src/index.tsx
@@ -23,3 +23,8 @@ const render = () => {
   });
 };
 render();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+// serviceWorkerRegistration.register(); //change the service worker registration from 'unregistered' to 'registered'
