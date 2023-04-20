8f1ce55c906a56fb9c05f44b9e91c5fba516cef9
MasterVHM <scrummaster@vhmsoluciones.com>
2023-04-20 07:25:41 -0500

    Fix load audit information

--- src/components/common/Data/CreateEditAuditInformation.tsx
+++ src/components/common/Data/CreateEditAuditInformation.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Loader} from '../../common/Loader';
+import { Loader } from '../../common/Loader';
 import { Input, Label } from 'reactstrap';
 import moment from 'moment';
 import IntlMessages from '../../../helpers/IntlMessages';
@@ -10,60 +10,60 @@ const CreateEditAuditInformation = (props: any) => {
 
   return (
     <>
-      <div >            
-          <div>
-            <IntlMessages id="forms.auditInformation" />
-            <div className="float-right row mr-0">
-              <Label>
-                <IntlMessages id="forms.version" />: {auditInfo.version}
-              </Label>
-            </div>
+      <div >
+        <div>
+          <IntlMessages id="forms.auditInformation" />
+          <div className="float-right row mr-0">
+            <Label>
+              <IntlMessages id="forms.version" />: {auditInfo.version}
+            </Label>
           </div>
-          <div>
-            {loading ? (
-              <Colxx sm={12} className="d-flex justify-content-center">
-                <Loader/>
-              </Colxx>
-            ) : (
-              <>
-                <Label />
-                <div className="d-flex">
-                  {/* Created At */}
-                  <Label className="form-group has-float-label">
-                    <Input defaultValue={moment(auditInfo.createdAt).format('YYYY-MM-DD h:mm a')} readOnly={true} />
-                    <span>
-                      <IntlMessages id="forms.createdAt" />
-                    </span>
-                  </Label>
-                  <div className="p-2" />
-                  {/* Created By */}
-                  <Label className="form-group has-float-label">
-                    <Input defaultValue={auditInfo.createdBy} readOnly={true} />
-                    <span>
-                      <IntlMessages id="forms.createdBy" />
-                    </span>
-                  </Label>
-                </div>
-                <div className="d-flex">
-                  {/* Updated At */}
-                  <Label className="form-group has-float-label">
-                    <Input defaultValue={moment(auditInfo.updatedAt).format('YYYY-MM-DD h:mm a')} readOnly={true} />
-                    <span>
-                      <IntlMessages id="forms.updatedAt" />
-                    </span>
-                  </Label>
-                  <div className="p-2" />
-                  {/* Updated By */}
-                  <Label className="form-group has-float-label">
-                    <Input defaultValue={auditInfo.updatedBy} readOnly={true} />
-                    <span>
-                      <IntlMessages id="forms.updatedBy" />
-                    </span>
-                  </Label>
-                </div>
-              </>
-            )}
-          </div>     
+        </div>
+        <div>
+          {loading ? (
+            <Colxx sm={12} className="d-flex justify-content-center">
+              <Loader />
+            </Colxx>
+          ) : (
+            <>
+              <Label />
+              <div className="d-flex">
+                {/* Created At */}
+                <Label className="form-group has-float-label">
+                  <Input defaultValue={moment(auditInfo.createdAt).format('YYYY-MM-DD h:mm a')} readOnly={true} />
+                  <span>
+                    <IntlMessages id="forms.createdAt" />
+                  </span>
+                </Label>
+                <div className="p-2" />
+                {/* Created By */}
+                <Label className="form-group has-float-label">
+                  <Input defaultValue={auditInfo.createdByUser?.name} readOnly={true} />
+                  <span>
+                    <IntlMessages id="forms.createdBy" />
+                  </span>
+                </Label>
+              </div>
+              <div className="d-flex">
+                {/* Updated At */}
+                <Label className="form-group has-float-label">
+                  <Input defaultValue={moment(auditInfo.updatedAt).format('YYYY-MM-DD h:mm a')} readOnly={true} />
+                  <span>
+                    <IntlMessages id="forms.updatedAt" />
+                  </span>
+                </Label>
+                <div className="p-2" />
+                {/* Updated By */}
+                <Label className="form-group has-float-label">
+                  <Input defaultValue={auditInfo.updatedByUser?.name} readOnly={true} />
+                  <span>
+                    <IntlMessages id="forms.updatedBy" />
+                  </span>
+                </Label>
+              </div>
+            </>
+          )}
+        </div>
       </div>
     </>
   );
