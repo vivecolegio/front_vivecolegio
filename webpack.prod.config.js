42158e8c7e7b97afe0b01b823eddba93b839a3e4
MasterVHM <scrummaster@vhmsoluciones.com>
2023-10-23 14:55:04 -0500

    update ssl validation

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -82,8 +82,8 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env':{
         'NODE_ENV': JSON.stringify('production'),
-        'PUBLIC_URL': JSON.stringify('http://vivecolegios.nortedesantander.gov.co')
-      }
+        'PUBLIC_URL': JSON.stringify('http://vivecolegios.nortedesantander.gov.co'),
+      },
     }),
     new MiniCssExtractPlugin({
       filename: 'css/[name].[chunkhash].css',
@@ -106,33 +106,34 @@ module.exports = {
       // these options encourage the ServiceWorkers to get in there fast
       // and not allow any straggling "old" SWs to hang around
       clientsClaim: true,
-      skipWaiting: true
+      skipWaiting: true,
     }),
     new CopyPlugin({
       patterns: [
-        { from: path.resolve(__dirname, 'public/apple-icon-57x57.png'), to: path.resolve(__dirname, 'dist')},
-        { from: path.resolve(__dirname, 'public/apple-icon-60x60.png'), to: path.resolve(__dirname, 'dist')},
-        { from: path.resolve(__dirname, 'public/apple-icon-72x72.png'), to: path.resolve(__dirname, 'dist')},
-        { from: path.resolve(__dirname, 'public/apple-icon-76x76.png'), to: path.resolve(__dirname, 'dist')},
-        { from: path.resolve(__dirname, 'public/apple-icon-114x114.png'), to: path.resolve(__dirname, 'dist')},
-        { from: path.resolve(__dirname, 'public/apple-icon-120x120.png'), to: path.resolve(__dirname, 'dist')},
-        { from: path.resolve(__dirname, 'public/apple-icon-144x144.png'), to: path.resolve(__dirname, 'dist')},
-        { from: path.resolve(__dirname, 'public/apple-icon-152x152.png'), to: path.resolve(__dirname, 'dist')},
-        { from: path.resolve(__dirname, 'public/apple-icon-180x180.png'), to: path.resolve(__dirname, 'dist')},
-        { from: path.resolve(__dirname, 'public/android-icon-192x192.png'), to: path.resolve(__dirname, 'dist')},
-        { from: path.resolve(__dirname, 'public/android-icon-144x144.png'), to: path.resolve(__dirname, 'dist')},
-        { from: path.resolve(__dirname, 'public/android-icon-96x96.png'), to: path.resolve(__dirname, 'dist')},
-        { from: path.resolve(__dirname, 'public/android-icon-72x72.png'), to: path.resolve(__dirname, 'dist')},
-        { from: path.resolve(__dirname, 'public/android-icon-48x48.png'), to: path.resolve(__dirname, 'dist')},
-        { from: path.resolve(__dirname, 'public/android-icon-36x36.png'), to: path.resolve(__dirname, 'dist')},
-        { from: path.resolve(__dirname, 'public/favicon-32x32.png'), to: path.resolve(__dirname, 'dist')},
-        { from: path.resolve(__dirname, 'public/favicon-96x96.png'), to: path.resolve(__dirname, 'dist')},
-        { from: path.resolve(__dirname, 'public/favicon-16x16.png'), to: path.resolve(__dirname, 'dist')},
-        { from: path.resolve(__dirname, 'public/meta.json'), to: path.resolve(__dirname, 'dist')},
+        { from: path.resolve(__dirname, 'public/apple-icon-57x57.png'), to: path.resolve(__dirname, 'dist') },
+        { from: path.resolve(__dirname, 'public/apple-icon-60x60.png'), to: path.resolve(__dirname, 'dist') },
+        { from: path.resolve(__dirname, 'public/apple-icon-72x72.png'), to: path.resolve(__dirname, 'dist') },
+        { from: path.resolve(__dirname, 'public/apple-icon-76x76.png'), to: path.resolve(__dirname, 'dist') },
+        { from: path.resolve(__dirname, 'public/apple-icon-114x114.png'), to: path.resolve(__dirname, 'dist') },
+        { from: path.resolve(__dirname, 'public/apple-icon-120x120.png'), to: path.resolve(__dirname, 'dist') },
+        { from: path.resolve(__dirname, 'public/apple-icon-144x144.png'), to: path.resolve(__dirname, 'dist') },
+        { from: path.resolve(__dirname, 'public/apple-icon-152x152.png'), to: path.resolve(__dirname, 'dist') },
+        { from: path.resolve(__dirname, 'public/apple-icon-180x180.png'), to: path.resolve(__dirname, 'dist') },
+        { from: path.resolve(__dirname, 'public/android-icon-192x192.png'), to: path.resolve(__dirname, 'dist') },
+        { from: path.resolve(__dirname, 'public/android-icon-144x144.png'), to: path.resolve(__dirname, 'dist') },
+        { from: path.resolve(__dirname, 'public/android-icon-96x96.png'), to: path.resolve(__dirname, 'dist') },
+        { from: path.resolve(__dirname, 'public/android-icon-72x72.png'), to: path.resolve(__dirname, 'dist') },
+        { from: path.resolve(__dirname, 'public/android-icon-48x48.png'), to: path.resolve(__dirname, 'dist') },
+        { from: path.resolve(__dirname, 'public/android-icon-36x36.png'), to: path.resolve(__dirname, 'dist') },
+        { from: path.resolve(__dirname, 'public/favicon-32x32.png'), to: path.resolve(__dirname, 'dist') },
+        { from: path.resolve(__dirname, 'public/favicon-96x96.png'), to: path.resolve(__dirname, 'dist') },
+        { from: path.resolve(__dirname, 'public/favicon-16x16.png'), to: path.resolve(__dirname, 'dist') },
+        { from: path.resolve(__dirname, 'public/meta.json'), to: path.resolve(__dirname, 'dist') },
         { from: path.resolve(__dirname, 'public/manifest.json'), to: path.resolve(__dirname, 'dist') },
         { from: path.resolve(__dirname, 'public/12AE5AFDD31FAF4E046C893C8A1E55AE.txt'), to: path.resolve(__dirname, 'dist/.well-known/pki-validation/') },
-        { from: path.resolve(__dirname, 'public/C37DD54BE84B26F0027B9EB840D14002.txt'), to: path.resolve(__dirname, 'dist/.well-known/pki-validation/') }
-      ]
+        { from: path.resolve(__dirname, 'public/C37DD54BE84B26F0027B9EB840D14002.txt'), to: path.resolve(__dirname, 'dist/.well-known/pki-validation/') },
+        { from: path.resolve(__dirname, 'public/6B3D7EA45EF1FDCEE3F3D90DADAEF90B.txt'), to: path.resolve(__dirname, 'dist/.well-known/pki-validation/') },
+      ],
     }),
     // new AddAssetHtmlPlugin({
     //   filepath: path.resolve(__dirname, 'dist/js/*.dll.js'),
